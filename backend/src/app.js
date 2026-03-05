732ba108a457e947ed94cacd8a03df91913fd5fb
navaloli007 <navaloli007@gmail.com>
2026-03-05 11:07:18 +0530

    Revert "Api for signup, login, profile and request is created and tested."
    This reverts commit 1ed57582fd4417d5de0aa272f42228fc020460c0.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,11 @@
-const express = require("express");
-const connectDB = require("./config/database");
+const express = require('express');
 const app = express();
-const cookieParser = require("cookie-parser");
 
-app.use(express.json());
-app.use(cookieParser());
+app.use("/", (req, res) => {
+    res.send("Hello World!");
+});
 
-const authRouter = require("./routes/auth");
-const profileRouter = require("./routes/profile");
-const requestRouter = require("./routes/request");
 
-app.use("/", authRouter);
-app.use("/", profileRouter);
-app.use("/", requestRouter);
-
-connectDB()
-  .then(() => {
-    console.log("Database connection established...");
-    app.listen(7777, () => {
-      console.log("Server is successfully listening on port 7777...");
-    });
-  })
-  .catch((err) => {
-    console.error("Database cannot be connected!!");
-  });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
--- backend/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    // await mongoose.connect("mongodb+srv://navaloli007:jmb66j1K4GOT1D9Z@cluster0.fjs2y.mongodb.net/?appName=Cluster0");
-    await mongoose.connect("mongodb+srv://navaloli007:jmb66j1K4GOT1D9Z@cluster0.fjs2y.mongodb.net/devtinder");
-    
-};
-
-module.exports = connectDB;
