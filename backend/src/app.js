25fe12ac5f1b3fcdb21e57c2852765471efb5196
navaloli007 <navaloli007@gmail.com>
2026-03-20 14:42:59 +0530

    Login, Logout, Signup, Profile view/edit APIs.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,26 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
+const cookieParser = require("cookie-parser");
+require('dotenv').config();
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/", (req, res) => {
-    res.send("Hello World!");
-});
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(process.env.PORT, () => {
+      console.log("Server is successfully listening on port " + process.env.PORT + "...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
