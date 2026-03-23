a38f1279dd7f1415c1d2390c1461d5053ea364ce
navaloli007 <navaloli007@gmail.com>
2026-03-23 18:45:40 +0530

    Profile Interested and Ignored.

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -41,11 +41,10 @@ const userSchema = new mongoose.Schema(
         },
         gender: {
             type: String,
-            validate(value) {
-                if (!["male", "female", "others"].includes(value)) {
-                    throw new Error("Gender data is not valid");
-                }
-            },
+            enum: {
+                values: ["male", "female", "others"],
+                message: `{VALUE} is not a valid gender type`
+            }
         },
         photoUrl: {
             type: String,
--- backend/src/routes/request.js
+++ backend/src/routes/request.js
@@ -2,10 +2,46 @@ const express = require("express");
 const requestRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-    const user = req.user;
-    res.send(user.firstName + "sent the connect request!");
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+    try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const allowedSTatus = ["ignored", "interested"];
+
+        if (!allowedSTatus.includes(status)) {
+            return res.status(400).json({ error: "Invalid status type :" + status });
+        }
+
+        const toUser = await User.findById(toUserId);
+        if (!toUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const existingRequest = await ConnectionRequest.findOne({
+            $or: [
+                { fromUserId, toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId }
+            ],
+        });
+        if (existingRequest) {
+            return res.status(400).json({ error: "Connection request already sent to this user!" });
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status,
+        });
+        const data = await connectionRequest.save();
+        res.json({ message: req.user.firstName + " is " + status + " in " + toUser.firstName, data });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
 module.exports = requestRouter;
