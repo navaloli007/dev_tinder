da586cf50ceb0cdd58f24d6b1ced14cb56725d45
navaloli007 <navaloli007@gmail.com>
2026-03-31 16:09:23 +0530

    Add Redux, Login process.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,15 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+var cors = require('cors')
 require('dotenv').config();
+
+var corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
