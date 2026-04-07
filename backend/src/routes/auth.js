a5ba8e6e860bb721ef24bf6cf613146d4039508d
navaloli007 <navaloli007@gmail.com>
2026-04-07 12:40:40 +0530

    feat: SignUp flow and some bug fixes.

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -15,9 +15,12 @@ authRouter.post("/signup", async (req, res) => {
             emailId,
             password: passwordHash,
         });
-
-        await user.save();
-        res.send("User Added successfully!");
+        const savedUser = await user.save();
+        const token = await savedUser.getJWT();
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 8 * 3600000),
+        });
+        res.json({ message: "User Added successfully!", data: savedUser });
     } catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
